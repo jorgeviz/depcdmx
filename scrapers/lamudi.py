de80682f603965e4f5dd379828c4f2eb4d9384ff
Carl Handlin <cwallaceh@gmail.com>
2019-05-24 16:16:44 -0500

    Changed Lamudi scraper to match the information in the new Inmuebles24 scraper

--- scrapers/lamudi.py
+++ scrapers/lamudi.py
@@ -1,22 +1,34 @@
 """
-Module to scrape Lamudi DF appartments
+Module to scrape Lamudi
 and stores data in local storage as CSV.
 
 Fetched Fields:
-name, location, description, price, rooms, toilettes, surface, link 
+name, description, location, link, price, operation, rooms, bathrooms, construction (m2), terrain (m2)
 """
 import requests
+import statistics
 import pandas as pd
 from bs4 import BeautifulSoup
-import time
-import random
-from pprint import pprint as pp
 import datetime as dt
+import random
+import time
+import os
 
 # Vars
-_base_url = "https://www.lamudi.com.mx/distrito-federal/departamento/for-sale/?page={}"
-user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36"
+_root = "https://www.lamudi.com.mx/"
+_state = 'nuevo-leon'
+_operation = 'venta'
+_base_url = _root + _state + "/for-sale/?page={}"
 ddir = 'data/'
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    'referrer': 'https://google.com',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Pragma': 'no-cache',
+}
 
 
 def save(depts):
@@ -24,31 +36,31 @@ def save(depts):
 
         Params:
         -----
-        depts : list
-            List of Departments
+        depts : pd.Dataframe()
+            Dataframe of Departments
     """
-    # Read file if exists
-    _fname = ddir+"{}/inmuebles24.csv".format(dt.date.today().isoformat())
+    # Read Existant file to append
+    _fname = ddir + "{}/lamudi" + "-" + _state + "-" + _operation + ".csv"
+    _fname = _fname.format(dt.date.today().isoformat())
     try:
-        df = pd.read_csv(_fname, delimiter='~')
+        df = pd.read_csv(_fname, delimiter=',')
     except:
         print('New file, creating folder..')
         try:
-            os.mkdir(ddir+'{}'.format(dt.date.today().isoformat()))
+            os.mkdir(ddir + '{}'.format(dt.date.today().isoformat()))
             print('Created folder!')
         except:
             print('Folder exists already!')
         df = pd.DataFrame()
     # Append data
-    depdf = pd.DataFrame(depts)
-    print(depdf.head(1).to_dict())
+    print(depts.head(1).to_dict())
     try:
         if df.empty:
-            depdf.set_index(['name','location']).to_csv(_fname, sep='~')
+            depts.set_index(['name', 'location']).to_csv(_fname, sep=',')
             print('Correctly saved file: {}'.format(_fname))
         else:
-            df = pd.concat([df, depdf])
-            df.set_index(['name','location']).to_csv(_fname, sep='~')
+            df = pd.concat([df, depts])
+            df.set_index(['name', 'location']).to_csv(_fname, sep=',')
             print('Correctly saved file: {}'.format(_fname))
     except Exception as e:
         print(e)
@@ -56,45 +68,48 @@ def save(depts):
 
 
 def scrape(content):
-    """ Scrape all departments per page
-         
-        Params:
-        -----
-        content: str
-            HTML content
-    """
-    data = []
+    """ Scrape all listings per page """
+    columns = ['name',
+               'description',
+               'location',
+               'link',
+               'price',
+               'operation',
+               'rooms',
+               'bathrooms',
+               'construction (m2)',
+               'terrain (m2)']
+
+    data = pd.DataFrame(columns=columns)
     # Generate soup
     soup = BeautifulSoup(content, 'html.parser')
-    with open(ddir+'lamudi.html', 'w') as _F:
-        _F.write(soup.prettify())
-    for d in soup.find_all(class_="card ListingCell-content"):
-        _dept = {}
-        _dept['name'] = d.find(class_="ListingCell-KeyInfo-title").text.strip()
-        _dept['location'] = ' '.join(j.strip() for j in d.find(class_="ListingCell-KeyInfo-address ellipsis").text.strip().split('\n'))
-        _dept['description'] = d.find(class_="ListingCell-shortDescription").text.strip().replace(u'\xa0', u'').replace('\n',' ')
+    # Get Characteristics
+    for d in soup.find_all(class_="ListingCell-AllInfo"):
+        temp_dict = {}
         try:
-            _dept['price'] = d.find(class_="PriceSection-FirstPrice").text.replace('$','').replace(',','').strip()
-        except:
-            try:
-                _dept['price'] = d.find(class_="PriceSection-SecondPrice").text.replace('$','').replace(',','').replace('US','').strip()
-            except Exception as e:
-                _dept['price'] = ''
-        _rooms, _toils, _surf = '-', '-', '-'
-        for at in d.find_all(class_="KeyInformation-attribute"):
-            _label = at.find(class_='KeyInformation-label').text.strip()
-            _val = at.find(class_="KeyInformation-value").text.strip()
-            if 'Rec' in _label:
-                _rooms = _val
-            elif 'Baño' in _label:
-                _toils = _val
-            elif 'Superf' in _label:
-                _surf = _val
-        _dept['rooms'],_dept['toilettes'],_dept['surface'] = _rooms, _toils, _surf
-        _dept['link'] = d.find('a')['href']
-        # Append Data
-        data.append(_dept)    
+            temp_dict['name'] = d.find(class_="ListingCell-KeyInfo-title").text.strip()
+            temp_dict['description'] = d.find(class_="ListingCell-shortDescription").text.strip()
+            temp_dict['location'] = ' '.join([j.strip() for j in d.find(class_="ListingCell-KeyInfo-address").text.strip().split('\n')])
+            temp_dict['link'] = d.find(class_="ListingCell-KeyInfo-title").find('a').get('href')
+            temp_dict['price'] = d.find(class_="PriceSection-FirstPrice").text.strip()
+            temp_dict['operation'] = _operation
+            for att in d.find(class_="KeyInformation").find_all(class_="KeyInformation-attribute"):
+                att = att.text.lower()
+                if 'recámara' in att:
+                    temp_dict['rooms'] = statistics.mean([int(s) for s in att.split() if s.isdigit()])
+                elif 'baño' in att:
+                    temp_dict['bathrooms'] = statistics.mean([int(s) for s in att.split() if s.isdigit()])
+                elif 'terreno' in att:
+                    temp_dict['terrain (m2)'] = statistics.mean([int(s) for s in att.split() if s.isdigit()])
+                elif 'superficie' in att:
+                    temp_dict['construction (m2)'] = statistics.mean([int(s) for s in att.split() if s.isdigit()])
+        except Exception as e:
+            print(e)
+            continue
+        data = data.append(temp_dict, ignore_index=True)
+    print('Found {} depts'.format(len(data['name'])))
     return data
+
 
 def paginate():
     """ Loop over pages to retrieve all info available
@@ -109,14 +124,13 @@ def paginate():
         try:
             print(_base_url.format(pg_nums))
             r = requests.get(_base_url.format(pg_nums),
-                headers={'user-agent': user_agent})
+                             headers=headers)
             # Anti blocking delay
-            time.sleep(random.randint(0,5))
+            time.sleep(random.randint(5, 10))
             if r.status_code != 200:
                 raise Exception("Wrong Response")
             depts = scrape(r.content)
-            print('Found {} departments'.format(len(depts)))
-            if not depts:
+            if depts.empty:
                 raise Exception("No more departments")
         except Exception as e:
             print(e)
@@ -127,11 +141,12 @@ def paginate():
         pg_nums += 1
     return pg_nums
 
+
 def main():
-    """ Main method
-    """
+    """ Main method """
     print('Starting to scrape Lamudi')
-    pages = paginate()
+    paginate()
+    return "Done"
 
 
 if __name__ == '__main__':
